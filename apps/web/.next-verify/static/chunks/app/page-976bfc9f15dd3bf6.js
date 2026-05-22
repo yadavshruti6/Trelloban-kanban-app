c512f04380d49983222d4b4bfbb1fb72dc9866bf
Shruti Yadav <2023kuec2061@iiitkota.ac.in>
2026-05-23 05:02:19 +0530

    Remove build files from repository

--- apps/web/.next-verify/static/chunks/app/page-976bfc9f15dd3bf6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5575:function(n,e,u){Promise.resolve().then(u.bind(u,3087))}},function(n){n.O(0,[906,955,293,528,744],function(){return n(n.s=5575)}),_N_E=n.O()}]);
